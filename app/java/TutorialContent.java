2eae80e53af732f3455c3d5815adc8d871286601
anushi5 <anushitomar5@gmail.com>
2017-07-21 19:17:03 +0530

    Committed

---
+++ app/java/TutorialContent.java
@@ -0,0 +1,14 @@
+package com.example.anushi.hackrio;
+
+/**
+ * Created by anushi on 12/7/17.
+ */
+
+public class TutorialContent {
+
+    public String tut_name;
+    public String submitted_by;
+    public String upvotes;
+    public String source;
+    public String status;
+}
